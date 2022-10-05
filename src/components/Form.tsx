cd3cfb6ec38b154d7a70f69f09599d5b05586069
alexdietz1988 <alex.dietz@gmail.com>
2022-10-05 11:45:39 -0400

    Toggle date buttons

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,25 +3,57 @@ function Form(props: any) {
     const decadeButtons = []
     const yearButtons = []
 
+    function earlierButton() {
+      let buttonClass = 'button'
+      let clickHandler = () => props.setDates({start: -700, end: 1699})
+      if (props.dates.end < 1700) {
+        buttonClass += ' is-link'
+        clickHandler = () => props.setDates(props.defaultDates)
+      }
+      return (
+        <button className={buttonClass} onClick={clickHandler}>
+          Earlier
+        </button>
+      )
+    }
+
     for (let i = 1700; i < 2100; i += 100) {
+      let buttonClass = 'button'
+      let clickHandler = () => props.setDates({start: i, end: i + 99})
+      if (props.dates.start >= i && props.dates.end <= i + 99) {
+        buttonClass += ' is-link'
+        clickHandler = () => {props.setDates(props.defaultDates)}
+      }
       centuryButtons.push(
-        <button key={i} className='button' onClick={() => props.setDates({start: i, end: i + 99})}>
+        <button key={i} className={buttonClass} onClick={clickHandler}>
           {i}s
         </button>)
     }
   
     let firstYearOfCentury = Math.floor(props.dates.start/100) * 100
     for (let i = firstYearOfCentury; i < firstYearOfCentury + 99 && i < 2030; i += 10) {
+      let buttonClass = 'button'
+      let clickHandler = () => props.setDates({start: i, end: i + 9})
+      if (props.dates.start >= i && props.dates.end <= i + 9) {
+        buttonClass += ' is-link'
+        clickHandler = () => {props.setDates({start: firstYearOfCentury, end: firstYearOfCentury + 99})}
+      }
       decadeButtons.push(
-        <button key={i} className='button' onClick={() => props.setDates({start: i, end: i + 9})}>
+        <button key={i} className={buttonClass} onClick={clickHandler}>
           {i}s
         </button>)
     }
   
     let firstYearOfDecade = Math.floor(props.dates.start/10) * 10
     for (let i = firstYearOfDecade; i < firstYearOfDecade + 10 && i < 2023; i++) {
+      let buttonClass = 'button'
+      let clickHandler = () => { props.setDates({start: i, end: i}) }
+      if (props.dates.start === i && props.dates.end === i) {
+        buttonClass += ' is-link'
+        clickHandler = () => {props.setDates({start: firstYearOfDecade, end: firstYearOfDecade + 9})}
+      }
       yearButtons.push(
-        <button key={i} className='button' onClick={() => props.setDates({start: i, end: i})}>
+        <button key={i} className={buttonClass} onClick={clickHandler}>
           {i}
         </button>)
     }
@@ -29,7 +61,7 @@ function Form(props: any) {
     return (
         <>
         <div className='buttons has-addons'>
-        <button className='button' onClick={() => props.setDates({start: -700, end: 1699})}>Earlier</button>
+        {earlierButton()}
         {centuryButtons}
         </div>
 
@@ -47,17 +79,15 @@ function Form(props: any) {
         : null
         }
 
-        <div className='buttons has-addons'>
-          {props.author === '' ?
-            <button className='button'>All Authors</button> :
+        {props.author === '' ?
+          null :
+          <div className='buttons has-addons'>
             <button className='button is-info' onClick={() => props.setAuthor('')}>
               {props.author}
               <button className='delete is-small ml-1'></button>
             </button>
-          }
-          
-          <button className='button' onClick={() => props.setDates(props.defaultDates)}>All Dates</button>
-        </div>
+          </div>
+        }
         </>
     )
 }
