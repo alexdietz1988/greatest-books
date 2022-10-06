42bb941b325da8928ee63dfc0584329514818502
alexdietz1988 <alex.dietz@gmail.com>
2022-10-06 11:44:53 -0400

    Refactor BooksDisplay

--- src/components/BooksDisplay.tsx
+++ src/components/BooksDisplay.tsx
@@ -1,55 +1,26 @@
 import { useEffect, useState } from 'react'
-import fiction from '../fiction'
-import nonfiction from '../nonfiction'
+import fiction from '../data/fiction'
+import nonfiction from '../data/nonfiction'
 
 function BooksDisplay(props: any) {
     const [books, setBooks] = useState(fiction)
+    useEffect(filterBooks, [props])
 
     function filterBooks(): void {
         let allBooks = props.genre === 'fiction' ? fiction : nonfiction
-        let filtered = []
+        const filtered = []
         for (let i = 0; i < allBooks.length && filtered.length < 100; i++) {
-          let book = allBooks[i]
-          if (
-            (book.year > props.dates.start - 1 && book.year < props.dates.end + 1) &&
+            let book = allBooks[i]
+            if ((book.year > props.dates.start - 1 && book.year < props.dates.end + 1) &&
             (props.author === '' || book.author === props.author)) {
             filtered.push(book)
-          }
+            }
         }
         setBooks(filtered)
     }
-    
-    useEffect(filterBooks, [props])
 
-    const booksDisplay = books.map((book, idx): JSX.Element => {
-        return (
-          <tr key={idx}>
-            <td>{book.rank}</td>
-            <td onClick={() => {
-                props.setAuthor(book.author)
-                props.setDates(props.defaultDates)
-            }}>
-                <a>{book.author}</a>
-            </td>
-            <td><em>{book.title}</em></td>
-            <td onClick={() => {
-                if (typeof book.year === "number") {
-                    props.setAuthor('')
-                    props.setDates({ start: book.year, end: book.year })
-                }
-                }}>
-                <a>{book.year}</a>
-            </td>
-          </tr>
-        )
-        }
-    )
-
-    if (books.length === 0) {
-        return <div>No results</div>
-    }
-
-    return (
+    return books.length === 0 ?
+        <p>No results</p> :
         <table className='table' style={{maxWidth: "600px"}}>
             <thead>
                 <tr>
@@ -59,9 +30,26 @@ function BooksDisplay(props: any) {
                     <th className='is-narrow'>Date</th>
                 </tr>
             </thead>
-            {booksDisplay}
+
+            { books.map((book, idx): JSX.Element => (
+                <tr key={idx}>
+                    <td>{book.rank}</td>
+                    <td onClick={() => {
+                        props.setAuthor(book.author)
+                        props.setDates(props.defaultDates)
+                    }}>
+                        <a>{book.author}</a>
+                    </td>
+                    <td><em>{book.title}</em></td>
+                    <td onClick={() => {
+                        props.setAuthor('')
+                        props.setDates({ start: book.year, end: book.year })
+                        }}>
+                        <a>{book.year}</a>
+                    </td>
+                </tr>
+            )) }
         </table>
-    )
 }
 
 export default BooksDisplay
